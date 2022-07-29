869e3d95046133936a06a505e9aa6a33d3d78a9a
Dranoel224 <ringetbong@gmail.com>
2022-07-29 15:26:45 +0100

    LEVEL TWO FIRST TEST

---
+++ javascript/miniproject2.js
@@ -0,0 +1,13 @@
+let containerEl = document.getElementById("container");
+
+for(let counter = 0; counter <=50; counter+=5){
+    if(counter%10 == 0? inflate():normal()){
+        
+    };
+}
+
+
+
+function inflate(){
+    document.querySelector("container").style.hover
+}
