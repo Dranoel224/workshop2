b49ba730e7883d2f51f91fb5ee643f58dd43aade
Dranoel224 <ringetbong@gmail.com>
2022-07-29 16:27:18 +0100

    LEVEL TWO FIRST TEST

--- javascript/miniproject2.js
+++ javascript/miniproject2.js
@@ -5,7 +5,7 @@ let containerEl = document.getElementById("container").onmouseenter = function()
 
     let counter = 0;
 for(; counter <=100; counter+=5){
-    if(counter%10 == 0? containerEl():normal()){
+    if(counter%10 == 0? counter:undefined){
         console.log(counter);
     }
     return counter;
@@ -23,5 +23,4 @@ containerEl = document.getElementById("container").onmouseout = function(){
     this.style.backgroundColor = "green";
     this.style.border = "5px solid orange";
     this.style.transform = "scale(1.0)";
-
 }
