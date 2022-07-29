f14e0a1082a13659d0b13f895593f24fa6ff5c30
Dranoel224 <ringetbong@gmail.com>
2022-07-29 16:17:27 +0100

    LEVEL TWO FIRST TEST

--- javascript/miniproject2.js
+++ javascript/miniproject2.js
@@ -1,17 +1,27 @@
-let containerEl = document.getElementById("container").onmouseover = function(){
+let containerEl = document.getElementById("container").onmouseenter = function(){
     this.style.backgroundColor = "green";
     this.style.border = "5px solid orange";
     this.style.transform = "scale(1.1)";
 
     let counter = 0;
 for(; counter <=100; counter+=5){
-    if(counter%10 == 0? inflate():normal()){
+    if(counter%10 == 0? containerEl():normal()){
         console.log(counter);
     }
     return counter;
 }
 }
 
-function inflate(){
-    document.querySelector("container");
+containerEl = document.getElementById("container").onmouseleave = function(){
+    this.style.backgroundColor = "green";
+    this.style.border = "5px solid orange";
+    this.style.transform = "scale(1.0)";
+
+}
+
+containerEl = document.getElementById("container").onmouseout = function(){
+    this.style.backgroundColor = "green";
+    this.style.border = "5px solid orange";
+    this.style.transform = "scale(1.0)";
+
 }
